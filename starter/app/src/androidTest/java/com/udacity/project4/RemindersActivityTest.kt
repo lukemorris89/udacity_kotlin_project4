4f0366e740a0292ba1b00d1f6a1fe2bb61671265
lukemorrisbond <68226155+lukemorrisbond@users.noreply.github.com>
2021-05-15 10:47:15 +0100

    Added RemindersActivity end-to-end tests.

--- starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ starter/app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,31 @@
 package com.udacity.project4
 
 import android.app.Application
+import android.os.SystemClock
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.longClick
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -63,5 +78,49 @@ class RemindersActivityTest :
         runBlocking {
             repository.deleteAllReminders()
         }
+    }
+
+    @Test
+    fun createNewReminder() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        onView(withId(R.id.noDataTextView)).check(matches(isDisplayed()))
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.reminderTitle)).perform(
+            ViewActions.typeText("TestReminder"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.reminderDescription)).perform(
+            ViewActions.typeText("TestDescription"),
+            ViewActions.closeSoftKeyboard()
+        )
+        onView(withId(R.id.selectLocation)).perform(click())
+        onView(withId(R.id.map)).perform(longClick())
+        onView(withId(R.id.location_name_et)).perform(
+            ViewActions.typeText("TestLocationName"),
+            ViewActions.closeSoftKeyboard()
+        )
+
+        onView(withId(R.id.create_reminder_button)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withText("TestReminder")).check(matches(isDisplayed()))
+        onView(withText("TestDescription")).check(matches(isDisplayed()))
+        onView(withText("TestLocationName")).check(matches(isDisplayed()))
+
+        activityScenario.close()
+        }
+
+    @Test
+    fun createNewReminder_noLocation_returnsError() {
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+
+        onView(withId(R.id.addReminderFAB)).perform(click())
+        onView(withId(R.id.saveReminder)).perform(click())
+
+        onView(withText("Please select location")).check(matches(isDisplayed()))
+
+        activityScenario.close()
     }
 }
